00990025dde86ac658bfd00144e10b1bbd937093
Maia McGuinness <maiamcguinness@gmail.com>
2014-11-14 16:53:01 -0600

    Removing the rogue css sourcemap, updating the settings to not produce said sourcemaps, and removing the extra line from the css/.gitignore file

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,6 +13,7 @@ module.exports = function(grunt) {
         sass: {
             dist: {
                 options: {
+                    sourcemap: 'none',
                     style: 'expanded'
                 },
                 files: {
